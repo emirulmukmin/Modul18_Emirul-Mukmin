edde9aab8f25ab0dc743accd7ccbcb09a531e94a
emirulmukmin <82020237+emirulmukmin@users.noreply.github.com>
2022-11-22 09:28:05 +0700

    Add files via upload

---
+++ src/pages/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
+const NotFound = () => {
+  return (
+    <div>Halaman tidak ditemukan</div>
+  )
+}
+
+export default NotFound
