f2ee73ec4f8c968e5572aa72c1067eb00ca62c2d
emirulmukmin <82020237+emirulmukmin@users.noreply.github.com>
2022-12-03 14:49:45 +0700

    update for not-found

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -3,7 +3,13 @@ import React from 'react'
 // Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
 const NotFound = () => {
   return (
-    <div>Halaman tidak ditemukan</div>
+    <>
+      <img src="https://i.imgur.com/qIufhof.png" />
+      <div id="info">
+          <h1>404 NOT FOUND</h1>
+          <h3>This page could not be found</h3>
+      </div>
+    </>
   )
 }
 
